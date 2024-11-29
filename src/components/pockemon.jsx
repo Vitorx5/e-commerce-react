49bb26492ed97fda6337f9e605c75158fd035200
João Vitor <MrTafax@Gmail.com>
2024-11-29 12:15:31 -0400

    Feat: Criei props para o componente Pockemon

--- src/components/pockemon.jsx
+++ src/components/pockemon.jsx
@@ -1,3 +1,4 @@
-export const NomeDoPokemon = () => {
-    return <h2>Nome do Pokemon: pickachu</h2>
-  };
+export const NomeDoPokemon = ({ name = 'Pockemon' }) => {
+
+    return <h2>Nome do Pokemon: {name}</h2>
+  };        
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,11 @@ import { NomeDoPokemon } from "../components/pockemon";
 export const Home =() => {
   return(<div>
     <h1>Olá Mundo!</h1>
-    <NomeDoPokemon/>
+    <NomeDoPokemon name="Pikachu"/>
+    <NomeDoPokemon name="Bulbasuar"/>
+    <NomeDoPokemon name="Charmander"/>
+    <NomeDoPokemon name="Squirtle"/>
+    <NomeDoPokemon name="Rattata"/>
     </div>);
 }
 
